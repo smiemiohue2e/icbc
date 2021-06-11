b01a3ab9f744b23dbec528bd794f6d0bdd8342a3
smiemiohue2e <shanglouqu8544@126.com>
2021-06-11 22:56:03 +0800

    个人信息html

---
+++ src/main/resources/static/js/autoLoss.js
@@ -0,0 +1,111 @@
+﻿//设置参数
+var schoolNo;   //学校编号
+var memberCode;   //学工号
+
+var cardStatus;
+
+$.ajaxSetup({
+    async: false //取消异步         
+});
+
+function GetQueryString(name) {                                       //获取浏览器地址栏参数的方法。
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]); return null;
+};
+
+$(function () {
+
+
+    //通过openID和学校编号获取 学工号
+    if (checkOpenID()) {
+
+        checkmcard();
+
+ //   memberCode = "WX0002"//getCookie("memberCode");             //这两步其实可以不需要了
+  //  addCookie("memberCode", memberCode, 0);
+
+    //通过学工号和学校编号获取用户信息显示
+      $("#autoLoss").on("click",lossConfirm);
+    }
+
+
+});
+
+var checkmcard = function () {
+    var ajaxPram = {};
+    ajaxPram["schoolNum"] = schoolNo;
+    ajaxPram["lostMemberCode"] = memberCode;
+    $.post("loserDetail.ashx", ajaxPram, function (response) {
+        var resp = eval("(" + response + ")");
+        var cardStatus = resp.body.data.list[0]["cardStatus"];
+        var mcardSelected = $("#mcard").find("option:selected").text()
+        var resp = eval("(" + response + ")");
+        var length = resp.body.data.list.length;
+        $select = $("#mcard");
+        if (length == 1) {
+            $("#memberName").attr("value", resp.body.data.list[0]["memberName"]);
+            $("#memberCode").attr("value", resp.body.data.list[0]["memberCode"]);
+            $("#deptDes").attr("value", resp.body.data.list[0]["deptDes"]);
+            $("#mcard").attr("value", resp.body.data.list[0]["mcard"]);
+            mcard = resp.body.data.list[0]["mcard"];
+           
+            $select.attr("disabled", "disabled");
+        } else {
+                for (var i = 0; i < resp.body.data.list.length; i++) {        
+                    if (resp.body.data.list[i]["mcard"] == mcardSelected) {
+                            $("#memberName").attr("value", resp.body.data.list[i]["memberName"]);
+                            $("#memberCode").attr("value", resp.body.data.list[i]["memberCode"]);
+                            $("#deptDes").attr("value", resp.body.data.list[i]["deptDes"]);                      
+                            mcard = resp.body.data.list[i]["mcard"];
+                            cardStatus = resp.body.data.list[i]["cardStatus"];
+                            $("#mcard").attr("value", resp.body.data.list[0]["mcard"]);
+                            break;
+                        
+                    } 
+                }
+            }
+
+        $("#cardStatus").attr("value", cardStatus);
+        if (cardStatus != "正常") {
+            //    $("#autoLoss").attr("class", "weui-btn weui-btn_disabled weui-btn_warn ");
+            $("#autoLoss").attr("disabled", "disabled");
+            $("#autoLoss").attr("style", "background:gray");   //
+        } else {
+            $("#autoLoss").removeAttr("disabled", "disabled");
+            $("#autoLoss").removeAttr("style", "background:gray");
+        }
+    });
+
+}
+
+var lossConfirm = function () {
+    var password = $("#password").val();
+    if (!password) {
+        //密码为空
+        $.alert("密码为空", "警告");
+        return false;
+    } else if (check_str(password)) {
+        $.alert("密码不能包含非法字符!", "警告");
+        return false;
+    } 
+    //通过学校编号，学工号，卡类型和一卡通密码 进行自助挂失
+
+    var ajaxPram2 = {};
+    ajaxPram2["schoolNo"] = schoolNo;
+    ajaxPram2["memberCode"] = memberCode;
+    ajaxPram2["mcard"] = "0";
+    ajaxPram2["password"] = $("#password").val();
+
+    $.post("autoLoss.ashx", ajaxPram2, function (response) {
+        var resp = eval("(" + response + ")");
+        if (resp.body.resp_code == "0") {
+            
+            $.toast("挂失成功", "success");
+        } else {
+            $.toast("挂失失败,密码是否正确？", "forbidden");
+        }
+        
+    });
+
+}
---
+++ src/main/resources/static/js/utils.js
@@ -0,0 +1,113 @@
+﻿var openID;  //
+var userID;
+var respzk;
+function GetQueryString(name) {                                       //获取浏览器地址栏参数的方法。
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]); return null;
+};
+$.ajaxSetup({
+    async: false //取消异步         
+});
+
+//银行卡
+var bankA;
+var bankB;
+var bankC;
+var bankD;
+
+//添加cookie
+function addCookie(name, value, expiresHours) {
+    var cookieString = name + "=" + escape(value);                     //escape()函数可以对字符串进行编码，这样就可以在所有的计算机上读取该字符串。
+    //判断是否设置过期时间,0代表关闭浏览器时失效
+    if (expiresHours > 0) {
+        var date = new Date();
+        date.setTime(date.getTime + expiresHours * 3600 * 1000);
+        cookieString = cookieString + "; expires=" + date.toGMTString();
+    }
+    document.cookie = cookieString;
+}
+
+//得到cookie
+function getCookie(c_name) {
+    if (document.cookie.length > 0) {
+        c_start = document.cookie.indexOf(c_name + "=");//这里因为传进来的的参数就是带引号的字符串，所以c_name可以不用加引号
+        if (c_start != -1) {
+            c_start = c_start + c_name.length + 1;
+            c_end = document.cookie.indexOf(";", c_start);//当indexOf()带2个参数时，第二个代表其实位置，参数是数字，这个数字可以加引号也可以不加（最好还是别加吧）
+            if (c_end == -1) c_end = document.cookie.length;
+            return unescape(document.cookie.substring(c_start, c_end));
+        }
+    }
+}
+
+//获取当前时间
+function getNowFormatDate() {
+    var date = new Date();
+
+    var month = date.getMonth() + 1;
+    var strDate = date.getDate();
+    var seconds = date.getSeconds();
+    seconds = seconds.toString();
+    var hours = date.getHours();
+
+    var minutes = date.getMinutes();
+
+    if (month >= 1 && month <= 9) {
+        month = "0" + month;
+    }
+    if (strDate >= 0 && strDate <= 9) {
+        strDate = "0" + strDate;
+    }
+    if (hours >= 0 && hours <= 9) {
+        hours = "0" + hours;
+    }
+    if (minutes >= 0 && minutes <= 9) {
+        minutes = "0" + minutes;
+    }
+
+    if (seconds >= 0 && seconds <= 9) {
+        seconds = "0" + seconds;
+    }
+
+    var currentdate = String(date.getFullYear()) + String(month) + String(strDate)
+             + String(hours) + String(minutes)
+             + String(seconds);
+
+    return currentdate;
+}
+
+
+
+//校验openId的方法
+var checkOpenID = function () {
+    
+    //通过openID和学校编号获取 学工号 并检测
+    userID = $("#hiddenArea").attr("data-UserId");
+    schoolNo = "NJIT";
+
+   
+   memberCode = userID;
+     
+    return 1;
+}
+
+//检测非法字符，用来密码输入的
+function check_str(str) {
+    var re_ck = "[@/'\"#$%&^*]+";
+    var strForText = str;
+    var reg = new RegExp(re_ck);
+    if (reg.test(strForText)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//延时重载函数
+function myrefresh() {
+
+    setInterval('window.location.reload()',3000);
+
+}
+
